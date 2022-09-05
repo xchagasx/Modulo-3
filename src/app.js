86d2ce06c6841c732a0e7148d1678fb89e521cfc
Jonathan Ferreira <j0n4t@hotmail.com>
2022-09-05 12:13:36 -0300

    fix: remove lógica para status
    de acordo com comentário em https://github.com/betrybe/live-lectures/pull/1874#discussion_r962815723, foi decidido deixar os endpoints de service refatorados na aula ao vivo não funcionais, para serem corrigidos no dia de controller.

--- src/app.js
+++ src/app.js
@@ -27,33 +27,25 @@ app.get('/drivers/open/travels', async (_req, res) => {
 app.put('/drivers/:driverId/travels/:travelId/assign', async (req, res) => {
   const { travelId, driverId } = req.params;
   const { type, message } = await driverService.travelAssign({ travelId, driverId });
+  if (type) return res.status(type).json(message);
   
-  if (type && type.endsWith('NOT_FOUND')) return res.status(404).json(message);
-  if (type === 'TRAVEL_CONFLICT') return res.status(409).json(message);
-  if (type === 'INVALID_VALUE') return res.status(422).json(message);
-  
-  return res.status(200).json(message);
+  res.status(200).json(message);
 });
 
 app.put('/drivers/:driverId/travels/:travelId/start', async (req, res) => {
   const { travelId, driverId } = req.params;
   const { type, message } = await driverService.startTravel({ travelId, driverId });
-  if (type && type.endsWith('NOT_FOUND')) return res.status(404).json(message);
-  if (type === 'TRAVEL_CONFLICT') return res.status(409).json(message);
-  if (type === 'INVALID_VALUE') return res.status(422).json(message);
-  
-  return res.status(200).json(message);
+  if (type) return res.status(type).json(message);
+
+  res.status(200).json(message);
 });
 
 app.put('/drivers/:driverId/travels/:travelId/end', async (req, res) => {
   const { travelId, driverId } = req.params;
   const { type, message } = await driverService.endTravel({ travelId, driverId });
-  
-  if (type && type.endsWith('NOT_FOUND')) return res.status(404).json(message);
-  if (type === 'TRAVEL_CONFLICT') return res.status(409).json(message);
-  if (type === 'INVALID_VALUE') return res.status(422).json(message);
-  
-  return res.status(200).json(message);
+  if (type) return res.status(type).json(message);
+
+  res.status(200).json(message);
 });
 
 module.exports = app;
