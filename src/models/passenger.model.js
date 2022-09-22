2fe47c20acce04e7af9cecfab8e980fd6bd3f3cf
Jonathan Ferreira <j0n4t@hotmail.com>
2022-09-22 12:46:59 -0300

    feat(model): `passengers` - implementação e testes

--- src/models/passenger.model.js
+++ src/models/passenger.model.js
@@ -1,5 +1,13 @@
 const camelize = require('camelize');
+const snakeize = require('snakeize');
 const connection = require('./connection');
+
+const findAll = async () => {
+  const [result] = await connection.execute(
+    'SELECT * FROM passengers',
+  );
+  return camelize(result);
+};
 
 const findById = async (passengerId) => {
   const [[passenger]] = await connection.execute(
@@ -9,6 +17,25 @@ const findById = async (passengerId) => {
   return camelize(passenger);
 };
 
+const insert = async (passenger) => {
+  const columns = Object.keys(snakeize(passenger))
+    .map((key) => `${key}`)
+    .join(', ');
+
+  const placeholders = Object.keys(passenger)
+    .map((_key) => '?')
+    .join(', ');
+
+  const [{ insertId }] = await connection.execute(
+    `INSERT INTO passengers (${columns}) VALUE (${placeholders})`,
+    [...Object.values(passenger)],
+  );
+
+  return insertId;
+};
+
 module.exports = {
+  findAll,
   findById,
+  insert,
 };
